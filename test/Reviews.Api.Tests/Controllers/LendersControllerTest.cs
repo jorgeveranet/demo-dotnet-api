bf44dbea970b5fa0773385c63835ca2fbdbd6a88
Jorge Vera <Jorge.Vera@bankrate.com>
2018-05-03 09:58:21 -0400

    Rename tests

--- test/Reviews.Api.Tests/Controllers/LendersControllerTest.cs
+++ test/Reviews.Api.Tests/Controllers/LendersControllerTest.cs
@@ -1,17 +1,20 @@
 using Reviews.Api.Controllers;
 using Xunit;
 using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
 
-namespace Reviews.Api.Tests
+namespace Reviews.Api.Tests.Controllers
 {
-    public class LendersControllerShould
+    public class LendersControllerTest
     {
         [Fact]
         public async void Get()
         {
             var controller = new LendersController();
-
             var response = await controller.Get();
+
+            var okResult = Assert.IsType<OkResult>(response);
+            Assert.Equal(200, okResult.StatusCode);
         }
     }
 }
---
+++ test/Reviews.Api.Tests/Controllers/ReviewsControllerTest.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Reviews.Api.Tests.Controllers
+{
+    public class ReviewsControllerTest
+    {
+        [Fact (Skip = "working on lender")]
+        public void Get()
+        {
+        }
+    }
+}
